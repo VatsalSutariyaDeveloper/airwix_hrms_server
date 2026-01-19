ffc7b6f662ffb8d0749f7f564a81f8dbaf28f929
Vatsal Sutariya <developervatalsutariya@gmail.com>
2026-01-19 11:29:57 +0530

    shift api created

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -2,14 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const { attendancePunch } = require("../controllers/attendance/attendanceController.js");
-const { shiftController } = require("../controllers/attendance/shiftController.js");
+const shiftController = require("../controllers/attendance/shiftController.js");
 const { weeklyOffController } = require("../controllers/attendance/weeklyOffController.js");
 
 router.post("/punch", attendancePunch);
 
 // Shift
-// router.post("/shift", shiftController.createShift);
-// router.post("/shift/assign", shiftController.assignShift);
+router.post("/shift", shiftController.create);
+router.get("/shift", shiftController.getAll);
+router.get("/shift/:id", shiftController.getById);
+router.put("/shift/:id", shiftController.update);
+router.delete("/shift/", shiftController.delete);
+router.put("/shift/", shiftController.updateStatus);
+router.post("/shift/assign", shiftController.assignShift);
 
 // Weekly Off
 // router.post("/weekly-off", weeklyOffController.setWeeklyOff);
