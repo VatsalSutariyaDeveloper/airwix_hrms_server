c9be3dc04badb12f818e97d71c9173950d894486
rajairwix-wq <raj.airwix@gmail.com>
2026-01-20 18:18:44 +0530

    update emplouee model

--- models/employee.js
+++ models/employee.js
@@ -1,39 +1,43 @@
 module.exports = (sequelize, DataTypes) => {
     const Employee = sequelize.define("Employee", {
-        // 1. PERSONAL INFORMATION
-        series_id: { type: DataTypes.INTEGER },
+
+        employee_type: {type: DataTypes.SMALLINT,defaultValue: 1, comment:"1:staff, 2:worker"},
+        role_id: { type: DataTypes.INTEGER, allowNull: true },
+
+        // PROFILE INFORMATION
+        profile_image: { type: DataTypes.STRING },
         employee_code: { type: DataTypes.STRING },
         first_name: { type: DataTypes.STRING },
-        father_name: { type: DataTypes.STRING },
-        role_id: { type: DataTypes.INTEGER, allowNull: true },
-        email: { type: DataTypes.STRING, validate: { isEmail: true } },
-        blood_group: { type: DataTypes.SMALLINT, comment: "1: A+, 2: A-, 3: B+, 4: B-, 5: O+, 6: O-, 7: AB+, 8: AB-" },
-        marital_status: { type: DataTypes.SMALLINT, comment: "1: Married, 2: Unmarried" },
-        marriage_date: { type: DataTypes.DATEONLY },
-        spouse_name: { type: DataTypes.STRING },
-        country_of_origin: { type: DataTypes.STRING, defaultValue: 'India' },
-        nationality: { type: DataTypes.STRING, defaultValue: 'Indian' },
-        is_international_emp: { type: DataTypes.BOOLEAN, defaultValue: false },
-        physically_challenged: { type: DataTypes.BOOLEAN, defaultValue: false },
-        disability_type: { type: DataTypes.SMALLINT, comment: "1: Hearing, 2: Locomotive disability, 3: Visual, 4: None" },
-        gender: { type: DataTypes.SMALLINT, comment: "1: Male, 2: Female, 3: Others" },
-        profile_image: { type: DataTypes.STRING },
         mobile_no: { type: DataTypes.STRING },
+        designation:{ type: DataTypes.STRING, allowNull:true },
+        attendance_supervisor:{ type: DataTypes.STRING, allowNull:true },
+        reporting_manager:{ type: DataTypes.STRING, allowNull:true },
+
+        // GENERAL INFORMATION
+        salary_cycle: { type: DataTypes.INTEGER },
+        weekly_off_template: { type: DataTypes.INTEGER },
+        holiday_template: { type: DataTypes.INTEGER },
+        leave_template: { type: DataTypes.INTEGER },
+        swift: { type: DataTypes.INTEGER },
+        attendance_weekly_off_template: { type: DataTypes.INTEGER },
+        geofence_template: { type: DataTypes.INTEGER },
+        attendance_setting_template: { type: DataTypes.INTEGER },
+        salary_access: { type: DataTypes.INTEGER },
+
+        // PERSONAL INFORMATION
+        gender: { type: DataTypes.SMALLINT, comment: "1: Male, 2: Female, 3: Others" },
         dob: { type: DataTypes.DATEONLY },
-        place_of_birth: { type: DataTypes.STRING },
-        height: { type: DataTypes.STRING },
-        weight: { type: DataTypes.STRING },
-        identification_mark: { type: DataTypes.STRING },
-        religion: { type: DataTypes.STRING },
-        caste: { type: DataTypes.STRING },
-        hobby: { type: DataTypes.STRING },
-
-        // 2. EMERGENCY CONTACT
-        emergency_contact_name: { type: DataTypes.STRING },
+        email: { type: DataTypes.STRING, validate: { isEmail: true } },
+        marital_status: { type: DataTypes.SMALLINT, comment: "1: Married, 2: Unmarried" },
+        blood_group: { type: DataTypes.SMALLINT, comment: "1: A+, 2: A-, 3: B+, 4: B-, 5: O+, 6: O-, 7: AB+, 8: AB-" },
+        physically_challenged: { type: DataTypes.BOOLEAN, defaultValue: false },
         emergency_contact_mobile: { type: DataTypes.STRING },
-        emergency_contact_relation: { type: DataTypes.SMALLINT, comment: "1: Brother, 2: Sister, 3: Father, 4: Mother, 5: Spouse, 6: Son, 7: Daughter" },
+        father_name: { type: DataTypes.STRING },
+        mother_name: { type: DataTypes.STRING, allowNull:true },
+        spouse_name: { type: DataTypes.STRING },
 
-        // ADDRESS
+        // ADDRESS INFORMATION
+        same_as_current: { type: DataTypes.BOOLEAN, defaultValue: false },
         permanent_address1: { type: DataTypes.STRING },
         permanent_address2: { type: DataTypes.STRING },
         permanent_city: { type: DataTypes.STRING },
@@ -50,47 +54,65 @@ module.exports = (sequelize, DataTypes) => {
         present_country_id: { type: DataTypes.STRING },
         present_address_proof_doc: { type: DataTypes.STRING },
 
-        // 4. JOINING & WORK STATUS
+        // EMPLOYEEMENT INFORMATION
         joining_date: { type: DataTypes.DATEONLY },
+        uan_number: { type: DataTypes.STRING },
+        name_as_per_pan: { type: DataTypes.STRING },
+        pan_number: { type: DataTypes.STRING },
+        name_as_per_aadhaar: { type: DataTypes.STRING },
+        aadhaar_number: { type: DataTypes.STRING },
+        pf_number: { type: DataTypes.STRING },
+        pf_joing_date: { type: DataTypes.DATEONLY },
+        pf_eligible: { type: DataTypes.BOOLEAN, defaultValue: false },
+        esi_eligible: { type: DataTypes.BOOLEAN, defaultValue: false },
+        esi_number: { type: DataTypes.INTEGER },
+        pt_eligible: { type: DataTypes.BOOLEAN, defaultValue: false },
+        lwf_eligible: { type: DataTypes.BOOLEAN, defaultValue: false },
+        eps_eligible: { type: DataTypes.BOOLEAN, defaultValue: false },
+        eps_joing_date: { type: DataTypes.DATEONLY },
+        eps_exit_date: { type: DataTypes.DATEONLY },
+        hps_eligible: { type: DataTypes.BOOLEAN, defaultValue: false },
+
+        // BANK INFORMATION
+        name_as_per_bank: { type: DataTypes.STRING },
+        bank_name: { type: DataTypes.STRING, allowNull:true },
+        bank_account_number: { type: DataTypes.STRING },
+        bank_ifsc_code: { type: DataTypes.STRING },
+        bank_account_holder_name: { type: DataTypes.STRING, allowNull:true },
+        upi_id:{ type: DataTypes.STRING, allowNull:true },
+ 
+        // DOCUMENT UPLOAD
+        aadhaar_doc: { type: DataTypes.STRING },
+        pan_doc: { type: DataTypes.STRING },
+        bank_proof_doc: { type: DataTypes.STRING },
+
+        // OTHER INFORMATION
+        marriage_date: { type: DataTypes.DATEONLY },
+        country_of_origin: { type: DataTypes.STRING, defaultValue: 'India' },
+        nationality: { type: DataTypes.STRING, defaultValue: 'Indian' },
+        is_international_emp: { type: DataTypes.BOOLEAN, defaultValue: false },
+        disability_type: { type: DataTypes.SMALLINT, comment: "1: Hearing, 2: Locomotive disability, 3: Visual, 4: None" },
+        place_of_birth: { type: DataTypes.STRING },
+        height: { type: DataTypes.STRING },
+        weight: { type: DataTypes.STRING },
+        identification_mark: { type: DataTypes.STRING },
+        religion: { type: DataTypes.STRING },
+        caste: { type: DataTypes.STRING },
+        hobby: { type: DataTypes.STRING },
+        
+        emergency_contact_name: { type: DataTypes.STRING },
+        emergency_contact_relation: { type: DataTypes.SMALLINT, comment: "1: Brother, 2: Sister, 3: Father, 4: Mother, 5: Spouse, 6: Son, 7: Daughter, 8: Other" },
+
         confirmation_date: { type: DataTypes.DATEONLY },
         probation_period_days: { type: DataTypes.INTEGER },
         notice_period_days: { type: DataTypes.INTEGER },
         referred_by: { type: DataTypes.STRING },
 
-        // 5. BANK ACCOUNT DETAILS
-        bank_country: { type: DataTypes.STRING, defaultValue: 'India' },
-        bank_account_number: { type: DataTypes.STRING },
-        bank_ifsc_code: { type: DataTypes.STRING },
-        name_as_per_bank: { type: DataTypes.STRING },
-        account_type: { type: DataTypes.SMALLINT, comment: "1: Savings, 2: Current, 3: Others" },
-        bank_proof_doc: { type: DataTypes.STRING },
-
-        // 6. STATUTORY & IDENTITIES
-        previous_pf_number: { type: DataTypes.STRING },
-        uan_number: { type: DataTypes.STRING },
-
-        // PAN
-        pan_number: { type: DataTypes.STRING },
-        name_in_pan: { type: DataTypes.STRING },
-        pan_doc: { type: DataTypes.STRING },
-
-        // Aadhaar
-        aadhaar_number: { type: DataTypes.STRING },
-        name_in_aadhaar: { type: DataTypes.STRING },
-        aadhaar_doc: { type: DataTypes.STRING },
-
-        // PRAN
-        pran_number: { type: DataTypes.STRING },
-        name_in_pran: { type: DataTypes.STRING },
-
-        // Passport
         passport_number: { type: DataTypes.STRING },
-        name_in_passport: { type: DataTypes.STRING },
+        name_as_per_passport: { type: DataTypes.STRING },
         passport_expiry_date: { type: DataTypes.DATEONLY },
         passport_doc: { type: DataTypes.STRING },
 
-        // 7. EDUCATION DETAILS
-        // Stores array of: { qualification, institute, from_date, to_date, grade, remarks }
         education_details: {
             type: DataTypes.JSONB,
             defaultValue: []
@@ -100,24 +122,20 @@ module.exports = (sequelize, DataTypes) => {
             defaultValue: null,
             comment: "Stores the [0.12, -0.45, ...] vector from DeepFace"
         },
-        status: {
-            type: DataTypes.SMALLINT,
-            defaultValue: 0,
-            comment: "0: Active, 1: Inactive, 2: Deleted"
-        },  
+
+        status: {type: DataTypes.SMALLINT, defaultValue: 0, comment: "0: Active, 1: Inactive, 2: Deleted"},  
         user_id: { type: DataTypes.INTEGER, allowNull: true },
         branch_id: { type: DataTypes.INTEGER, allowNull: true },
         company_id: { type: DataTypes.INTEGER, allowNull: true },
     }, {
         tableName: 'employees',
         timestamps: true,
-        underscored: true // Ensures snake_case for created_at, updated_at
+        underscored: true
     });
 
     Employee.associate = function (models) {
         Employee.belongsTo(models.User, { foreignKey: "user_id", as: "created_by" });
         Employee.hasOne(models.User, { foreignKey: "employee_id", as: "linked_user" });
-        Employee.belongsTo(models.SeriesTypeMaster, { foreignKey: "series_id", as: "series_type" });
         Employee.hasMany(models.EmployeeFamilyMember, { foreignKey: "employee_id", as: "family_members" });
         Employee.hasMany(models.AttendancePunch, { foreignKey: "employee_id", as: "attendance_punches" });
     };
